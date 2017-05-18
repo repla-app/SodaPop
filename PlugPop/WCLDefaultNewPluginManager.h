65fdec2f17883d0cbcb6182389e09e296ed084d1
Roben Kleene <robenk@gmail.com>
2017-05-17 21:59:12 -0400

    Fix null specifiers

--- PlugPop/WCLDefaultNewPluginManager.h
+++ PlugPop/WCLDefaultNewPluginManager.h
@@ -23,8 +23,8 @@ NS_ASSUME_NONNULL_END
 
 NS_ASSUME_NONNULL_BEGIN
 @interface WCLDefaultNewPluginManager : NSObject <DefaultPluginDataSource>
-- (instancetype)initWithDefaults:(_Nonnull id <DefaultsType> * _Nonnull)defaults;
-@property (nonatomic, assign, nullable) id <WCLDefaultNewPluginManagerDataSource> *dataSource;
+- (instancetype)initWithDefaults:(_Nonnull id <DefaultsType>)defaults;
+@property (nonatomic, assign, nullable) id <WCLDefaultNewPluginManagerDataSource> dataSource;
 @property (nonatomic, strong, nullable) WCLPlugin *defaultNewPlugin;
 @end
 NS_ASSUME_NONNULL_END

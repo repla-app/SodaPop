e7801b277a50ec10711233a4eacbba40b7cf8254
Roben Kleene <robenk@gmail.com>
2017-09-21 21:53:06 -0400

    Make MockPluginsManager initialize-able

--- PlugPopTests/Mocks/MockPluginsManager.swift
+++ PlugPopTests/Mocks/MockPluginsManager.swift
@@ -13,4 +13,22 @@ class MockPluginsManger: PluginsManager {
     var mockPluginsController: MockPluginsController {
         return pluginsController as! MockPluginsController
     }
+
+    convenience init(pluginsPaths: [String],
+                     copyTempDirectoryURL: URL,
+                     defaults: DefaultsType,
+                     userPluginsPath: String,
+                     builtInPluginsPath: String?)
+    {
+        let types = PluginsManagerConfigurationTypes(defaultNewPluginManagerType: WCLDefaultNewPluginManager.self,
+                                                     pluginsDataControllerType: PluginsDataController.self,
+                                                     pluginsControllerType: MockPluginsController.self)
+        let configuration = PluginsManagerConfiguration(types: types,
+                                                        pluginsPaths: pluginsPaths,
+                                                        copyTempDirectoryURL: copyTempDirectoryURL,
+                                                        defaults: defaults,
+                                                        userPluginsPath: userPluginsPath,
+                                                        builtInPluginsPath: builtInPluginsPath)
+        self.init(configuration: configuration)
+    }
 }

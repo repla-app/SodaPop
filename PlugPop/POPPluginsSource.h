b34de12eb34d87ab194bf889e01b90de15043638
Roben Kleene <contact@robenkleene.com>
2018-09-29 17:49:46 -0400

    Move methods to plugins source

--- PlugPop/POPPluginsSource.h
+++ PlugPop/POPPluginsSource.h
@@ -6,9 +6,16 @@
 //  Copyright © 2017 Roben Kleene. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
 @class Plugin;
 
+NS_ASSUME_NONNULL_BEGIN
 @protocol POPPluginsSource <NSObject>
+- (void)addPlugin:(Plugin *)plugin;
+- (void)removePlugin:(Plugin *)plugin;
+- (nullable Plugin *)pluginWithName:(NSString *)name;
+- (nullable Plugin *)pluginWithIdentifier:(NSString *)identifier;
 #pragma mark Required Key-Value Coding To-Many Relationship Compliance
 - (NSArray<Plugin *> *)plugins;
 - (void)insertObject:(Plugin *)plugin inPluginsAtIndex:(NSUInteger)index;
@@ -16,3 +23,4 @@
 - (void)removeObjectFromPluginsAtIndex:(NSUInteger)index;
 - (void)removePluginsAtIndexes:(NSIndexSet *)indexes;
 @end
+NS_ASSUME_NONNULL_END

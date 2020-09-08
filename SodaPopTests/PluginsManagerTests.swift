469c369051eb6f4fd6d5ee2b62486187566765bf
Roben Kleene <contact@robenkleene.com>
2020-09-08 17:55:55 -0400

    Add new test case file

---
+++ SodaPopTests/PluginsManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PluginsManagerTests.swift
+//  SodaPopTests
+//
+//  Created by Roben Kleene on 9/8/20.
+//  Copyright © 2020 Roben Kleene. All rights reserved.
+//
+
+import XCTest
+@testable import SodaPop
+import SodaPopTestHarness
+
+class PluginsManagerTests: PluginsManagerTestCase {
+    func testMultiplePluginsDirectories() {
+        
+    }
+}
